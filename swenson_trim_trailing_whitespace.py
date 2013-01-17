cd65fdd0b629c1f0f51d949b4ee5540612745ad5
Walt Woods <walt@sellerengine.com>
2013-01-17 14:22:40 -0800

    Don't open an edit for unchanged lines, since it moves the cursor and mucks up the undo history when you've been programming cleanly.

--- swenson_trim_trailing_whitespace.py
+++ swenson_trim_trailing_whitespace.py
@@ -44,23 +44,34 @@ class SwensonTrimTrailingWhiteSpace(sublime_plugin.EventListener):
         new_lines.remove(k)
     # Trim the whitespace on the new lines:
     if new_lines:
-      edit = view.begin_edit()
+      replacements = []
+
       for line_no in new_lines:
         pt = view.text_point(line_no, 0)
         old_line = view.line(pt)
-        new_line_text = view.substr(old_line).rstrip()
-        view.replace(edit, old_line, new_line_text)
-      view.end_edit(edit)
+        old_line_text = view.substr(old_line)
+        new_line_text = old_line_text.rstrip()
+        if old_line_text != new_line_text:
+          # Only add it if it actually changed, to prevent shifting e.g.
+          # the cursor on already groomed lines
+          replacements.append((old_line, new_line_text))
+
+      if replacements:
+        edit = view.begin_edit()
+        for old_line, new_line_text in replacements:
+          view.replace(edit, old_line, new_line_text)
+        view.end_edit(edit)
 
     # Trim all whitespace on my files.
     patterns = options.get('owner_patterns', [])
     if any(view.find(pattern, 0, sublime.IGNORECASE) for pattern in patterns):
       trailing_white_space = view.find_all("[\t ]+$")
-      trailing_white_space.reverse()
-      edit = view.begin_edit()
-      for r in trailing_white_space:
-          view.erase(edit, r)
-      view.end_edit(edit)
+      if trailing_white_space:
+        trailing_white_space.reverse()
+        edit = view.begin_edit()
+        for r in trailing_white_space:
+            view.erase(edit, r)
+        view.end_edit(edit)
 
   # Reload the file into the snapshot after saving.
   def on_post_save(self, view):
